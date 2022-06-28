6ff7dffc92622fd36f1ec16aaf183dbff0ed3a4d
Anna Laffoon <ree11194@gmail.com>
2022-06-28 15:23:26 -0400

    created get&post episodes endpoint to execute sql

--- server.js
+++ server.js
@@ -5,10 +5,22 @@ const sqlite = require('better-sqlite3');
 
 app.use(express.static("public"))
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+app.get('/episodes', (req, res) => {
+    let db = sqlite("who_reviews.db");
+    const stmt = db.prepare('SELECT * FROM Episodes');
+    const episodeData = stmt.all();
+    res.send(episodeData);
+})
+
+app.post('/episodes', (req, res) => {
+    //variables to represent the values for sql insert
+    const stmt = db.prepare('INSERT into reviews (Rating, Comment) values ( , )');
+    // const info = stmt.run(req.body.)//last property is whatever is in the body of the request
+    //execute sql insert
 })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+export default episodeData;

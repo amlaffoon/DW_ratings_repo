75d21493d69edb1ce936765f9a17f91f478c6c94
Anna Laffoon <ree11194@gmail.com>
2022-06-28 15:16:06 -0400

    init express and bettersqlite

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const sqlite = require('better-sqlite3');
+
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})

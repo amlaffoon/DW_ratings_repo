bdf9d11442e19a3919047b5737eab99cb78192bd
Anna Laffoon <ree11194@gmail.com>
2022-07-01 17:04:12 -0400

    get and post endpoints for ratings

--- server.js
+++ server.js
@@ -5,24 +5,41 @@ const path = require('path');
 const Database = require('better-sqlite3');
 const initializeDatabase = require('./db/initializeDatabase');
 
+
 initializeDatabase();
 //function to run seed scripts if db doesn't exist - or put in a separate module and reference here
 
 app.use(express.static("public"))
+app.use(express.json());
 
 
 app.get('/episodes', (req, res) => {
-    let db = new Database("DWR_db.db");
+    let db = new Database("DWR_db2.db");
     const stmt = db.prepare('SELECT * FROM Episodes');
     const episodeData = stmt.all();
     res.send(JSON.stringify(episodeData));
 })
 
-app.post('/episodes', (req, res) => {
+app.post('/ratings', (req, res) => {
+    let db = new Database("DWR_db2.db");
     //variables to represent the values for sql insert
-    const stmt = db.prepare('INSERT into reviews (Rating, Comment) values ( , )');
+    if (req.body.rating < 1 || req.body.rating > 10) {
+        res.send("Rating must be between 1 and 10");
+    }
+    else {
+        const stmt = db.prepare('INSERT into Ratings (Rating, Comment, Episode_Id) values (?, ?, ?)');
+        stmt.run(req.body.rating, req.body.comment, req.body.episodeId);
+        res.send("saved review");
+    }
     // const info = stmt.run(req.body.)//last property is whatever is in the body of the request
     //execute sql insert
+})
+
+app.get('/ratings', (req, res) => {
+    let db = new Database("DWR_db2.db");
+    const stmt = db.prepare('SELECT * FROM Ratings');
+    const ratingsData = stmt.all();
+    res.send(JSON.stringify(ratingsData));
 })
 
 app.listen(port, () => {
